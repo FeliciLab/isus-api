ec316028539d7b43784f56599f8efbfef43d86ce
victor <victor.magalhaesp@gmail.com>
2020-05-04 16:43:21 -0300

    #29 Adicionando novas categorias na arquitetura

--- app/Model/Wordpress/App.php
+++ app/Model/Wordpress/App.php
@@ -5,8 +5,9 @@ namespace App\Model\Wordpress;
 class App
 {
     const APP = [
-        'Educação' => [451, 452],
+        'Educação' => [451, 452, 453, 454, 442],
         'Pesquisa Científica' => [443, 450, 448, 446, 445, 449, 447],
-        'Minha Saúde' => [486, 487, 488]
+        'Minha Saúde' => [486, 487, 488],
+        'Boletins Epidemiológicos' => [2]
     ];
 }
