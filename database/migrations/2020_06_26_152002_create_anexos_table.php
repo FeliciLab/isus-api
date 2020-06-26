db1def54ceb2d989179ea0a4750436da733a2952
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2020-06-26 14:48:38 -0300

    #47 :sparkles: implementando sincronizaçao e retorno dos anexos em projetos

---
+++ database/migrations/2020_06_26_152002_create_anexos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnexosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anexos', function (Blueprint $table) {
+            $table->unsignedBigInteger('projeto_id');
+            $table->text('link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anexos');
+    }
+}
