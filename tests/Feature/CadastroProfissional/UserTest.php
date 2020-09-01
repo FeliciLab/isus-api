20c1f47fe2c88b398bd93f034537a481e0343ab2
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2020-09-01 15:25:12 -0300

    #72 implementando testes cadastro user

--- tests/Feature/CadastroProfissional/UserTest.php
+++ tests/Feature/CadastroProfissional/UserTest.php
@@ -2,33 +2,54 @@
 
 namespace Tests\Feature;
 
+use App\Model\CategoriaProfissional;
+use App\Model\Estado;
+use App\Model\UnidadeServico;
 use App\Model\User;
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
     private function getUser()
     {
+        $this->seed();
+
+        $fakerBrasil = new Generator();
+        $fakerBrasil->addProvider(new \Faker\Provider\pt_BR\Person($fakerBrasil));
+
+        $faker = Factory::create();
+        $estado = Estado::find($faker->numberBetween(1, 27));
+        $municipio = $estado->municipios()->first();
+
+        $unidadesDeServico = UnidadeServico::whereNotNull('pai');
+        $unidades = $unidadesDeServico->first();
+
+        $categoriasProfissional = CategoriaProfissional::all();
+        $categoriaProfissional = $categoriasProfissional->first();
         return [
-            'email' => 'teste@gmail.com',
-            'nomeCompleto' => 'Victor Mag',
+            'email' => $faker->email,
+            'nomeCompleto' => $faker->name(),
             'senha' => '12345678',
             'repetirsenha' => '12345678',
-            'telefone' => '9999999999',
-            'cpf' => '12345678900',
-            'rg' => '12345678900',
-            'estadoId' => '6',
-            'estado' => 'Ceará',
-            'cidadeId' => '1347',
-            'cidade' => 'Fortaleza',
+            'telefone' => $faker->randomNumber(9),
+            'cpf' => $fakerBrasil->cpf(false),
+            'rg' => $fakerBrasil->rg(false),
+            'estadoId' => $estado->id,
+            'estado' => $estado->nome,
+            'cidadeId' => $municipio->id,
+            'cidade' => $municipio->nome,
             'termos' => 'true',
-            'categoriaProfissional' => '{"id":1,"nome":"Categoria 1"}',
+            'categoriaProfissional' => json_encode($categoriaProfissional),
             'titulacaoAcademica' => '[{"id":1,"nome":"Titulação 1"},{"id":2,"nome":"Titulação 2"}]',
             'tipoContratacao' => '[{"id":1,"nome":"Estatutário"},{"id":2,"nome":"Cooperado"}]',
             'instituicao' => '[{"id":1,"nome":"ESP 1"},{"id":2,"nome":"HGF 2"}]',
-            'unidadeServico' => '[{"id":5,"nome":"Pronto-socorro"},{"id":6,"nome":"Ambulatório"}]'
+            'unidadeServico' => json_encode([$unidades])
         ];
     }
 
