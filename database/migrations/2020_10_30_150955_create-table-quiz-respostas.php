10904ceffd259e24acb53a8d1f1c9b5e628c7e35
Chicão Thiago <fthiagogv@gmail.com>
2020-10-30 15:23:46 -0300

    migration: cria tabela qquiz_resposta

--- database/migrations/2020_10_30_150955_create-table-quiz-respostas.php
+++ database/migrations/2020_10_30_150955_create-table-quiz-respostas.php
@@ -13,7 +13,24 @@ class CreateTableQuizRespostas extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('qquiz_respostas', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('quiz_id');
+            $table->foreing('quiz_id')->references('qquiz_quiz')->on('id');
+
+            $table->unsignedBigInteger('questao_id');
+            $table->foreing('questao_id')->references('qquiz_questao')->on('id');
+
+            $table->unsignedBigInteger('questao_alternativa_id');
+            $table->foreing('questao_alternativa_id')->references('qquiz_questao_alternativa')->on('id');
+
+            $table->text('identificacao');
+            $table->text('tipo_identificacao');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -23,6 +40,6 @@ class CreateTableQuizRespostas extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('qquiz_respostas');
     }
 }
