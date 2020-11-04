605532de552f6015b2c6d3da110e2d9ce6d73159
Chicão Thiago <fthiagogv@gmail.com>
2020-11-04 15:22:54 -0300

    #120 - modelo alternativa questao

---
+++ app/Domains/QualiQualiz/Models/AlternativaQuestao.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Domains\QualiQualiz\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AlterrnativaQuestao extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'qquiz_alternativas_questoes';
+
+    protected $fillabel = [
+        'id',
+        'ordem_alternativa',
+        'alternativa',
+        'url_imagem',
+        'pontuacao',
+        'questao_id',
+        'ordem',
+    ];
+
+    public function questao()
+    {
+        return $this->belongsTo('App\Domains\QualiQualiz\Models\Questao');
+    }
+
+    public function respostas()
+    {
+        return $this->hasMany('App\Domains\QualiQualiz\Models\Respota');
+    }
+}
