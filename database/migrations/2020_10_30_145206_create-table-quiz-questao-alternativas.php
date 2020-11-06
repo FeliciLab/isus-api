f0d7977b1b12efa4e7c1b1802846ae157e9ee4ec
Francisco Thiago <fthiagogv@gmail.com>
2020-11-06 11:34:58 -0300

    Feature/qualiquiz migrations (#122)
    * removido e reinstalado composer.lock por incosistência na instalação
    
    * Criando migration para a tabela qquiz_quiz
    
    * migration: criar tabela qquiz_questoes
    
    * Criada migration p/ gerar tabela de alternativas no banco
    
    * migration: cria tabela qquiz_resposta
    
    * migration: criando tabela qquiz_quiz_questoes many to many
    
    * Corrigindo syntaxes e nomeclaturas nas migrations
    
    * #115 - migrations quali-quiz
    
    * #115 - adicionada coluna de ordem as questoes e alternativas
    
    * Removendo ordem da tabela de questões e adicionando na tabela de quiz-questoes
    
    * corrigindo falha
    
    * atualizando migrations para colocar alguns campos nulos
    
    * correções de migration

---
+++ database/migrations/2020_10_30_145206_create-table-quiz-questao-alternativas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableQuizQuestaoAlternativas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qquiz_alternativas_questoes', function (Blueprint $table) {
+            $table->id();
+            $table->text('alternativa');
+            $table->text('url_imagem')->nullable();
+            $table->unsignedInteger('pontuacao');
+            $table->unsignedTinyInteger('ordem')->nullable();
+
+            $table->unsignedBigInteger('questao_id');
+            $table->foreign('questao_id')->references('id')->on('qquiz_questoes');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('qquiz_alternativas_questoes');
+    }
+}
---
+++ database/migrations/2020_10_30_152426_create-table-quiz-questao-many-many.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableQuizQuestaoManyMany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qquiz_quiz_questoes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('quiz_id');
+            $table->foreign('quiz_id')->references('id')->on('qquiz_quiz');
+
+            $table->unsignedBigInteger('questao_id');
+            $table->foreign('questao_id')->references('id')->on('qquiz_questoes');
+
+            $table->unsignedTinyInteger('ordem')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('qquiz_quiz_questoes');
+    }
+}
