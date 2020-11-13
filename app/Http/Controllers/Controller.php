9e3bdba18aaa216f1b3c3cae2e262835c253d112
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2020-11-13 12:06:59 -0300

    #41 - adicionando swagger e adicionando um endpoint documentado

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,6 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *      version="2",
+     *      title="iSUS documentação",
+     *      description="Descrição dos endpoints",
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * ),
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="API Server",
+     * ),
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public static function paginationResolver($data, $step, $total, $current_page)
