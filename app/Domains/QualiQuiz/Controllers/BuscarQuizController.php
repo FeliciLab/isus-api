036fda364fff0dd73e39b04004f93482e8e69e09
Chicão Thiago <fthiagogv@gmail.com>
2020-11-18 12:06:11 -0300

    #121 - Adicionando algumas docs de phpcs

--- app/Domains/QualiQuiz/Controllers/BuscarQuizController.php
+++ app/Domains/QualiQuiz/Controllers/BuscarQuizController.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace App\Domains\QualiQuiz\Controllers;
 
@@ -7,8 +8,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Domains\QualiQuiz\Repository\QuizRepository;
 
+/**
+ * Classe controller para rotas de busca do quiz
+ *
+ * @category Qualiquiz
+ * @package  Domain\Qualiquiz
+ * @author   Chicão Thiago <fthiagogv@gmail.com>
+ * @license  GPL-3 http://www.gnu.org/licenses/gpl-3.0.en.html
+ * @link     https://github.com/EscolaDeSaudePublica/isus-api/issues/120
+ */
 class BuscarQuizController extends Controller
 {
+    /**
+     * Busca o quiz a partir do código id do quiz
+     *
+     * @param $request        Request
+     * @param $quizRepository QuizRepository
+     * @param $codQuiz        Int
+     *
+     * @return mix
+     */
     public function buscarQuiz(
         Request $request,
         QuizRepository $quizRepository,
--- app/Domains/QualiQuiz/Controllers/RespostasQuizController.php
+++ app/Domains/QualiQuiz/Controllers/RespostasQuizController.php
@@ -1,6 +1,6 @@
 <?php
-declare(strict_types = 1);
 
+declare(strict_types=1);
 
 namespace App\Domains\QualiQuiz\Controllers;
 
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Validator;
 use App\Domains\QualiQuiz\Repository\QuizRepository;
 use App\Domains\QualiQuiz\Utils\JWTDecoder;
 
+/**
+ * Classe controller para rotas de salvar as respostas do quiz
+ *
+ * @category Qualiquiz
+ * @package  Domain\Qualiquiz
+ * @author   Chicão Thiago <fthiagogv@gmail.com>
+ * @license  GPL-3 http://www.gnu.org/licenses/gpl-3.0.en.html
+ * @link     https://github.com/EscolaDeSaudePublica/isus-api/issues/121
+ */
 class RespostasQuizController extends Controller
 {
     /**
@@ -23,30 +32,52 @@ class RespostasQuizController extends Controller
      *      }
      *    ]
      * }
+     *
+     * @param $request        Request
+     * @param $jwtDecoder     JWTDecoder
+     * @param $quizRepository QuizRepository
+     *
+     * @return mix
      */
-    public function registrar(Request $request, JWTDecoder $jwtDecoder, QuizRepository $quizRepository)
-    {
+    public function registrar(
+        Request $request,
+        JWTDecoder $jwtDecoder,
+        QuizRepository $quizRepository
+    ) {
         if (!$request->header('Authorization')) {
-            return response()->json(['message' => 'Token não enviado'], 403 );
+            return response()->json(['message' => 'Token não enviado'], 403);
         }
 
-        $autenticacao = $jwtDecoder->decoderPayload($request->header('Authorization'));
+        $autenticacao = $jwtDecoder->decoderPayload(
+            $request->header('Authorization')
+        );
+
         if (Validator::make($autenticacao->toArray(), ['email' => 'required'])->fails()) {
             return response()->json(['message' => 'Token inválido'], 403);
         }
 
-        $validacao = Validator::make($request->all(), [
-            'respostas' => 'required',
-            'respostas.*.quizId' => 'required',
-            'respostas.*.questaoId' => 'required',
-            'respostas.*.alternativaId' => 'required',
-        ]);
+        $validacao = Validator::make(
+            $request->all(),
+            [
+                'respostas' => 'required',
+                'respostas.*.quizId' => 'required',
+                'respostas.*.questaoId' => 'required',
+                'respostas.*.alternativaId' => 'required',
+            ]
+        );
 
         if ($validacao->fails()) {
-            return response()->json(['message' => $validacao->errors()->toArray()], 400);
+            return response()->json(
+                ['message' => $validacao->errors()->toArray()],
+                400
+            );
         }
 
-        $resultado = $quizRepository->registrarRespostas(collect($request->respostas), $autenticacao);
+        $resultado = $quizRepository->registrarRespostas(
+            collect($request->respostas),
+            $autenticacao
+        );
+
         return response()->json(
             ['message' => $resultado['msg']],
             $resultado['status']
