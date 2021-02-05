f573d522f9ff6c731835f9b53e915e3e0bb9d953
Rui Moreno <ruiguemo@gmail.com>
2021-02-05 19:19:14 -0300

    #141 - adicionando integração à API sus e retornando as categorias-Arquitetura

--- app/Http/Controllers/Api/SynchronizeController.php
+++ app/Http/Controllers/Api/SynchronizeController.php
@@ -12,15 +12,8 @@ use GuzzleHttp\Client;
 
 class SynchronizeController extends Controller
 {
-    public const WORDPRESS_ENDPOINT = [
-        'https://coronavirus.ceara.gov.br/wp-json/wp/v2/' => 0,
-        'https://sus.ce.gov.br/elmo/wp-json/wp/v2/' => 1,
-    ];
-
     public function index()
     {
-        $apps = App::APP;
-
         \DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         \DB::statement('TRUNCATE TABLE projetos');
         \DB::statement('TRUNCATE TABLE categorias_projetos');
@@ -28,23 +21,67 @@ class SynchronizeController extends Controller
         \DB::statement('TRUNCATE TABLE anexos');
         \DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 
-        foreach (self::WORDPRESS_ENDPOINT as $ENDPOINT => $index) {
+        foreach (App::WORDPRESS_ENDPOINT as $prefixo => $endpoint) {
             $client = new Client();
-            $res = $client->get($ENDPOINT . 'project_category/?per_page=100');
+            $res = $client->get($endpoint . 'project_category/?per_page=100');
             $categoriasAPI = json_decode($res->getBody(), false);
-            $categoriaId = null;
+
             foreach ($categoriasAPI as $categoria) {
                 $categoriaId = $categoria->id;
 
                 $client = new Client();
-                $res = $client->get($ENDPOINT . 'project_category/' . $categoria->id);
+                $res = $client->get($endpoint . 'project_category/' . $categoriaId);
                 $categoriaAPI = json_decode($res->getBody(), false);
-                $this->atualizaCategoriasDosProjetos($categoriaAPI, $index);
+
+                $categoria = new Categoria();
+                $categoria->term_id = $prefixo . $categoriaAPI->id;
+                $categoria->name = $categoriaAPI->name;
+                $categoria->slug = $categoriaAPI->slug;
+                $categoria->save();
 
                 $clientProjeto = new Client();
-                $resProjeto = $clientProjeto->get($ENDPOINT . 'project/?project_category=' . $categoriaId);
+                $resProjeto = $clientProjeto->get($endpoint . 'project/?project_category=' . $categoriaId);
                 $projetosAPI = json_decode($resProjeto->getBody(), false);
-                $categoriasProjetosTemp = $this->atualizaProjetosPorCategorias($projetosAPI, $index);
+
+                foreach ($projetosAPI as $post) {
+                    $projetoExiste = Projeto::find($prefixo . $post->id);
+                    if (!isset($projetoExiste)) {
+                        $projeto = new Projeto();
+                        $projeto->id = $prefixo . $post->id;
+                        $projeto->data = $post->date;
+                        $projeto->post_title = html_entity_decode($post->title->rendered, ENT_NOQUOTES, 'UTF-8');
+                        $projeto->slug = $post->slug;
+                        $projeto->content = $post->content->rendered;
+
+                        try {
+                            $clientImage = new Client();
+                            $resImagem = $clientImage->get($post->_links->{'wp:featuredmedia'}[0]->href);
+                            $imageAPI = json_decode($resImagem->getBody(), false);
+                            $projeto->image = $imageAPI->guid->rendered;
+                        } catch (\Exception $e) {
+                            $projeto->image = null;
+                        }
+
+                        $projeto->save();
+
+                        $clientAnexo = new Client();
+                        $resAnexo = $clientAnexo->get($post->_links->{'wp:attachment'}[0]->href);
+                        $anexosAPI = json_decode($resAnexo->getBody(), false);
+                        foreach ($anexosAPI as $anexoAPI) {
+                            $anexo = new Anexo();
+                            $anexo->projeto_id = $prefixo . $post->id;
+                            $anexo->link = $anexoAPI->guid->rendered;
+                            $anexo->save();
+                        }
+
+                        foreach ($post->project_category as $projetoCategoria) {
+                            $categoriasProjetosTemp[] = [
+                                'categoria_id' => $prefixo . $projetoCategoria,
+                                'projeto_id' => $projeto->id,
+                            ];
+                        }
+                    }
+                }
             }
 
             foreach ($categoriasProjetosTemp as $categoriaProjetoTemp) {
@@ -52,90 +89,6 @@ class SynchronizeController extends Controller
                 $categoriaProjeto->categoria_id = $categoriaProjetoTemp['categoria_id'];
                 $categoriaProjeto->projeto_id = $categoriaProjetoTemp['projeto_id'];
                 $categoriaProjeto->save();
-            }
-        }
-    }
-
-    private function atualizaCategoriasDosProjetos($categoriaAPI, $index)
-    {
-        $categoriaId = null;
-        if ($index === 0) {
-            $categoriaId = $categoriaAPI->id;
-            $categoriaId_length = strlen((string) $categoriaId);
-            $expo10 = 10 ** (2 + $categoriaId_length);
-            $categoriaId = $categoriaId + $expo10;
-        } else {
-            $categoriaId = $categoriaAPI->id;
-            $categoriaId_length = strlen((string) $categoriaId);
-            $expo10 = 2 * (10 ** (2 + $categoriaId_length));
-            $categoriaId = $categoriaId + $expo10;
-        }
-        $categoria = new Categoria();
-        $categoria->term_id = $categoriaId;
-        $categoria->name = $categoriaAPI->name;
-        $categoria->slug = $categoriaAPI->slug;
-        $categoria->save();
-    }
-
-    private function atualizaProjetosPorCategorias($projetosAPI, $index)
-    {
-        foreach ($projetosAPI as $post) {
-            $projetoExiste = Projeto::find($post->id);
-            if (!isset($projetoExiste)) {
-                $categoriaId = null;
-                foreach ($post->project_category as &$valueId) {
-                    if ($index === 0) {
-                        $categoriaId = $valueId;
-                        $categoriaId_length = strlen((string) $categoriaId);
-                        $expo10 = 10 ** (2 + $categoriaId_length);
-                        $categoriaId = $categoriaId + $expo10;
-                        $valueId = $categoriaId;
-                    } else {
-                        $categoriaId = $valueId;
-                        $categoriaId_length = strlen((string) $categoriaId);
-                        $expo10 = 2 * (10 ** (2 + $categoriaId_length));
-                        $categoriaId = $categoriaId + $expo10;
-                        $valueId = $categoriaId;
-                    }
-                }
-                unset($valueId);
-
-                $projeto = new Projeto();
-                $projeto->id = $post->id;
-                $projeto->data = $post->date;
-                $projeto->post_title = html_entity_decode($post->title->rendered, ENT_NOQUOTES, 'UTF-8');
-                $projeto->slug = $post->slug;
-                $projeto->content = $post->content->rendered;
-
-                try {
-                    $clientImage = new Client();
-                    $resImagem = $clientImage->get($post->_links->{'wp:featuredmedia'}[0]->href);
-                    $imageAPI = json_decode($resImagem->getBody(), false);
-                    $projeto->image = $imageAPI->guid->rendered;
-                } catch (\Exception $e) {
-                    $projeto->image = null;
-                }
-
-                $projeto->save();
-
-                $clientAnexo = new Client();
-                $resAnexo = $clientAnexo->get($post->_links->{'wp:attachment'}[0]->href);
-                $anexosAPI = json_decode($resAnexo->getBody(), false);
-                foreach ($anexosAPI as $anexoAPI) {
-                    $anexo = new Anexo();
-                    $anexo->projeto_id = $post->id;
-                    $anexo->link = $anexoAPI->guid->rendered;
-                    $anexo->save();
-                }
-
-                foreach ($post->project_category as $projetoCategoria) {
-                    $categoriasProjetosTemp[] = [
-                        'categoria_id' => $projetoCategoria,
-                        'projeto_id' => $projeto->id,
-                    ];
-                }
-
-                return $categoriasProjetosTemp;
             }
         }
     }
--- app/Model/Wordpress/App.php
+++ app/Model/Wordpress/App.php
@@ -4,13 +4,49 @@ namespace App\Model\Wordpress;
 
 class App
 {
-    public const APP = [
-        'Educação' => [451, 452, 453, 454, 442],
-        'Pesquisa Científica' => [443, 450, 448, 446, 445, 449],
-        'Minha Saúde' => [486, 487, 488],
-        'Boletins Epidemiológicos' => [2],
-        'Notícias' => [4],
-        'Biblioteca' => [13],
-        'Instruções' => [40],
+    public const PREFIXO_CORONAVIRUS = 100;
+    public const PREFIXO_SUS = 200;
+
+    public const WORDPRESS_ENDPOINT = [
+        self::PREFIXO_CORONAVIRUS => 'https://coronavirus.ceara.gov.br/wp-json/wp/v2/',
+        self::PREFIXO_SUS => 'https://sus.ce.gov.br/elmo/wp-json/wp/v2/',
     ];
+
+    public $apps = [
+        self::PREFIXO_CORONAVIRUS => [
+            'Educação' => [451, 452, 453, 454, 442],
+            'Pesquisa Científica' => [443, 450, 448, 446, 445, 449],
+            'Minha Saúde' => [486, 487, 488],
+            'Boletins Epidemiológicos' => [2],
+        ],
+        self::PREFIXO_SUS => [
+            'Notícias' => [4],
+            'Biblioteca' => [13],
+            'Instruções' => [40],
+        ],
+    ];
+
+    public function getApp()
+    {
+        $appTemp = [];
+
+        foreach ($this->apps as $fonte => $categorias) {
+            foreach ($categorias as $categoriaNome => $categoriasId) {
+                $categoriasIdTemp = [];
+                if ($fonte == self::PREFIXO_CORONAVIRUS) {
+                    foreach ($categoriasId as $categoriaId) {
+                        $categoriasIdTemp[] = self::PREFIXO_CORONAVIRUS . $categoriaId;
+                    }
+                } elseif ($fonte == self::PREFIXO_SUS) {
+                    foreach ($categoriasId as $categoriaId) {
+                        $categoriasIdTemp[] = self::PREFIXO_SUS . $categoriaId;
+                    }
+                }
+
+                $appTemp[$categoriaNome] = $categoriasIdTemp;
+            }
+        }
+
+        return $appTemp;
+    }
 }
