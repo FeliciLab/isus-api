7faf1d7380e3528aa3570e09c8efd08b9c2eca30
Francisco Thiago <fthiagogv@gmail.com>
2021-08-27 12:34:56 -0300

    Adicionando configurações de e-mail que haviam sido inseridas indevidamente direto no servidor de homologação

--- config/mail.php
+++ config/mail.php
@@ -41,6 +41,13 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
