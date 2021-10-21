4e639cc0024c73e8bf0e273ddd37459350cc44fa
Laysa Cunha <laysacunha@gmail.com>
2021-10-21 15:14:38 -0300

    🐛 altera consulta do qualiquiz para retornar resultado com base no quiz e no usuario

--- app/Domains/QualiQuiz/Repository/RespostaRepository.php
+++ app/Domains/QualiQuiz/Repository/RespostaRepository.php
@@ -82,6 +82,7 @@ class RespostaRepository
         $ultimoQuiz = DB::table('qquiz_respostas')
             ->selectRaw('MAX(created_at) as data')
             ->where('identificacao', '=', $identificacao)
+            ->where('quiz_id', '=', $idQuiz)
             ->first();
 
         return collect(
