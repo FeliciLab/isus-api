cdb33607df7b07b6dd0a2d1e4e093e17a5ea719a
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-23 15:30:24 -0300

    ✨ add SaguUserInfoController

---
+++ app/Http/Controllers/SaguUserInfoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SaguUserInfoController extends Controller
+{
+    public function index()
+    {
+        return 'teste';
+    }
+}
