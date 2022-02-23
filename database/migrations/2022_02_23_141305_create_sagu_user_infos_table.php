f8b5f8ff9f991f78273fd610a6cebaabab850538
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-23 14:34:35 -0300

    ✨ add CreateSaguUserInfosTable migratuin

---
+++ database/migrations/2022_02_23_141305_create_sagu_user_infos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaguUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sagu_user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('componente', 60);
+            $table->string('programa_residencia', 100);
+            $table->string('municipio_residencia', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sagu_user_infos');
+    }
+}
