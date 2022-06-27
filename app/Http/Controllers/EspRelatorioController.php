671240ef6b19740c16f7bdf9a87d8ccf51541bbd
fpontef <sobralense@gmail.com>
2022-06-27 16:22:18 -0300

    🩹 fix XLS report and DateTime

--- app/Http/Controllers/EspRelatorioController.php
+++ app/Http/Controllers/EspRelatorioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Exports\EspRelatorioExport;
+use App\Http\Exports\EspRelatorioDetalhadoExport;
 use App\Model\Esp\EspOferta;
 use App\Model\Esp\EspPresenca;
 use App\Model\Esp\EspUserInfo;
@@ -18,8 +19,12 @@ class EspRelatorioController extends Controller
         try {
             $report = [];
             $ofertasArray = [];
+            /* Carga Horária a contabilizar por presença */
+            $cHPorPresenca = 8;
 
             $buscaPorOfertaId = request('oferta');
+            $relatorioDetalhado = request('detalhado');
+
 
             if ($buscaPorOfertaId) {
                 $espOfertas = EspOferta
@@ -36,56 +41,123 @@ class EspRelatorioController extends Controller
                 $ofertasArray[] = $oferta->id;
             }
 
-            $espPresencas = EspPresenca
-                ::select(
-                    'user_id',
-                    'esp_oferta_id',
-                    DB::raw('count(esp_oferta_id) as count_presenca')
-                )
-                ->whereIn('esp_oferta_id', $ofertasArray)
-                ->groupBy('user_id', 'esp_oferta_id')
-                ->get();
+            /*
+            * Se tiver detalhado=sim na url:
+            * /api/esp/relatorio?detalhado=sim
+            * Será exibido um relatório com data/hora de cada presença.s
+            */
+            if ($relatorioDetalhado === 'sim') {
+                $espPresencasContagemGeral = EspPresenca
+                    ::select(
+                        'user_id',
+                        'esp_oferta_id',
+                        DB::raw('count(esp_oferta_id) as count_presenca')
+                    )
+                    ->whereIn('esp_oferta_id', $ofertasArray)
+                    ->groupBy('user_id', 'esp_oferta_id')
+                    ->get();
 
-            foreach ($espPresencas as $presenca) {
-                $espOferta = $espOfertas->where('id', '=', $presenca->esp_oferta_id)->first();
+                $espPresencas = EspPresenca
+                    ::select(
+                        'id',
+                        'user_id',
+                        'esp_oferta_id',
+                        'data'
+                    )
+                    ->whereIn('esp_oferta_id', $ofertasArray)
+                    ->get();
 
-                $espUserInfo = EspUserInfo::where('user_id', '=', $presenca->user_id)->first();
+                foreach ($espPresencas as $presenca) {
+                    $espPresencasContagemIndividual = $espPresencasContagemGeral
+                        ->where('esp_oferta_id', '=', $presenca->esp_oferta_id)
+                        ->where('user_id', '=', $presenca->user_id)
+                        ->first()
+                        ->count_presenca;
 
-                $user = User::where('id', '=', $presenca->user_id)->first();
+                    $espOferta = $espOfertas
+                        ->where('id', '=', $presenca->esp_oferta_id)->first();
 
-                /*
-                * Regra Presença carga horária:
-                * Cada unidade de presença (cada presenca) contabiliza 8h.
-                */
-                $totalPresencasNecessarias = $espOferta->carga_horaria / 8;
-                $chPresenca = $presenca->count_presenca * 8;
-                $percentualPresenca =
-                    ($presenca->count_presenca * 100) / $totalPresencasNecessarias;
-                $percentualFalta = 100 - $percentualPresenca;
+                    $espUserInfo = EspUserInfo::where('user_id', '=', $presenca->user_id)->first();
 
-                $report[] = [
-                    'ofertaNome' => $espOferta->nome,
-                    'ofertaCargaHoraria' => $espOferta->carga_horaria,
-                    'ofertaInicio' => $espOferta->inicio,
-                    'ofertaFim' => $espOferta->fim,
-                    'ofertaAtiva' => $espOferta->is_active
-                        ? 'ATIVA'
-                        : 'INATIVA',
-                    'userNome' => $user->name,
-                    'userCpf' => $user->cpf,
-                    'areaEsp' => $espUserInfo->area_esp,
-                    'areaOutros' => $espUserInfo->area_outros,
-                    // 'countPresenca' => $presenca->count_presenca,
-                    'chPresenca' => $chPresenca,
-                    'percentualPresenca' => number_format($percentualPresenca, 2),
-                    'percentualFalta' => number_format($percentualFalta, 2),
-                ];
+                    $user = User::where('id', '=', $presenca->user_id)->first();
+
+
+                    /* Cálculo da Carga Horária */
+                    $totalPresencasNecessarias = $espOferta->carga_horaria / $cHPorPresenca;
+                    $chPresenca = $espPresencasContagemIndividual * $cHPorPresenca;
+                    $percentualPresenca =
+                        ($espPresencasContagemIndividual * 100) / $totalPresencasNecessarias;
+                    $percentualFalta = 100 - $percentualPresenca;
+
+                    $report[] = [
+                        'ofertaNome' => $espOferta->nome,
+                        'ofertaCargaHoraria' => $espOferta->carga_horaria,
+                        'ofertaInicio' => $espOferta->inicio,
+                        'ofertaFim' => $espOferta->fim,
+                        'ofertaAtiva' => $espOferta->is_active
+                            ? 'ATIVA'
+                            : 'INATIVA',
+                        'userNome' => $user->name,
+                        'userCpf' => $user->cpf,
+                        'areaEsp' => $espUserInfo->area_esp,
+                        'areaOutros' => $espUserInfo->area_outros,
+                        'dataPresenca' => $presenca->data->format('d-m-Y H:i:s'),
+                        'chPresenca' => $chPresenca,
+                        'percentualPresenca' => number_format($percentualPresenca, 2),
+                        'percentualFalta' => number_format($percentualFalta, 2),
+                    ];
+                }
+
+                $export = new EspRelatorioDetalhadoExport($report);
+            } else {
+                $espPresencas = EspPresenca
+                    ::select(
+                        'user_id',
+                        'esp_oferta_id',
+                        DB::raw('count(esp_oferta_id) as count_presenca')
+                    )
+                    ->whereIn('esp_oferta_id', $ofertasArray)
+                    ->groupBy('user_id', 'esp_oferta_id')
+                    ->get();
+
+                foreach ($espPresencas as $presenca) {
+                    $espOferta = $espOfertas->where('id', '=', $presenca->esp_oferta_id)->first();
+
+                    $espUserInfo = EspUserInfo::where('user_id', '=', $presenca->user_id)->first();
+
+                    $user = User::where('id', '=', $presenca->user_id)->first();
+
+                    /* Cálculo da Carga Horária */
+                    $totalPresencasNecessarias = $espOferta->carga_horaria / $cHPorPresenca;
+                    $chPresenca = $presenca->count_presenca * $cHPorPresenca;
+                    $percentualPresenca =
+                        ($presenca->count_presenca * 100) / $totalPresencasNecessarias;
+                    $percentualFalta = 100 - $percentualPresenca;
+
+                    $report[] = [
+                        'COUNT_PRESENCA' => $presenca->count_presenca,
+                        'ofertaNome' => $espOferta->nome,
+                        'ofertaCargaHoraria' => $espOferta->carga_horaria,
+                        'ofertaInicio' => $espOferta->inicio,
+                        'ofertaFim' => $espOferta->fim,
+                        'ofertaAtiva' => $espOferta->is_active
+                            ? 'ATIVA'
+                            : 'INATIVA',
+                        'userNome' => $user->name,
+                        'userCpf' => $user->cpf,
+                        'areaEsp' => $espUserInfo->area_esp,
+                        'areaOutros' => $espUserInfo->area_outros,
+                        'chPresenca' => $chPresenca,
+                        'percentualPresenca' => number_format($percentualPresenca, 2),
+                        'percentualFalta' => number_format($percentualFalta, 2),
+                    ];
+                }
+
+                $export = new EspRelatorioExport($report);
             }
 
-            $export = new EspRelatorioExport($report);
-
-            // return Excel::download($export, 'RelatorioPresencaEsp.xlsx');
-            return response()->json($report, 200);
+            // return response()->json($report, 200);
+            return Excel::download($export, 'RelatorioPresencaEsp.xlsx');
         } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
